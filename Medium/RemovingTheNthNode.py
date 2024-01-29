01326709aca75c8b6896bb70e7385171c3aab524
Adomas Fiseris <131570795+Adomas1992@users.noreply.github.com>
2024-01-28 22:20:48 -0600

    1_28_24v1

---
+++ Medium/RemovingTheNthNode.py
@@ -0,0 +1,29 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+from tkinter.tix import ListNoteBook
+
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        """
+        :type head: ListNode
+        :type n: int
+        :rtype: ListNode
+        """
+        dummy = ListNoteBook(0, head)
+        fast = slow = dummy
+
+        for i in range(n):
+            fast = fast.next
+
+        while fast.next:
+            fast = fast.next
+            slow = slow.next
+
+        slow.next = slow.next.next
+
+        return dummy.next
+    
